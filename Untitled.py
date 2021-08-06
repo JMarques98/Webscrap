1ad9321b218cb6873dd38e456fc7cc7d7faed13e
Julio José Marqués <87929993+JMarques98@users.noreply.github.com>
2021-08-06 12:19:53 +0200

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -26,35 +26,35 @@ url = 'https://coinmarketcap.com/es'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#CRIPTOMONEDA 
+#CRYPTOCURRENCY 
 
 cr = soup.find_all('p', class_ = 'sc-1eb5slv-0 iJjGCS')
 
-criptomonedas = list()
+crypto = list()
 
 for i in cr: 
-    criptomonedas.append(i.text)
+    crypto.append(i.text)
 
 
 # In[15]:
 
 
-#VALOR 
+#VALUE OF THE COIN
 
 vl = soup.find_all('div', class_ = 'sc-131di3y-0 cLgOOr')
 
-valores = list()
+values = list()
 
 for i in vl:
-    valores.append(i.text)
+    values.append(i.text)
 
 
 # In[16]:
 
 
-#Se guarda en un dataframe
+#Save the Dataframe
 
-df = pd.DataFrame({'Name': criptomonedas, 'Value':valores})
+df = pd.DataFrame({'Name': crypto, 'Value':values})
 print(df)
 
 
